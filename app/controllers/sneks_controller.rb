7313f49916871e77f294e48ebd76aa7c9cd22b80
Hilllary Gospodarek <hgospodarek@everquote.com>
2017-04-28 21:40:12 -0400

    * Basic index, create, show for Sneks * Sneks index spec * Sneks factory

--- app/controllers/sneks_controller.rb
+++ app/controllers/sneks_controller.rb
@@ -2,6 +2,27 @@ class SneksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @sneks = current_user.sneks
+    @sneks = current_user.sneks.order(:name)
+    @snek = Snek.new
+  end
+
+  def create
+    @snek = Snek.new(snek_params)
+    @snek.user = current_user
+    if @snek.save
+      redirect_to @snek, notice: "#{@snek.name} succsessssfully created."
+    else
+      render action: 'index', error: "There were troubles creating your snek, please try again."
+    end
+  end
+
+  def show
+    @snek = Snek.find(params[:id])
+  end
+
+  private
+
+  def snek_params
+    params.require(:snek).permit(:name, :age, :adoption_date)
   end
 end
